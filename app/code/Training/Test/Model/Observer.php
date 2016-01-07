6af0392af8595a974bda7c6f11ff35326abbc56e
Alexey <alexey@Alexeys-MacBook-Pro.local>
2016-01-08 01:42:03 +0200

    All tasks Topic 1 done

---
+++ app/code/Training/Test/Model/Observer.php
@@ -0,0 +1,23 @@
+<?php
+namespace Training\Test\Model;
+
+use Magento\Framework\Event\ObserverInterface;
+
+class Observer implements ObserverInterface
+{
+
+    /**
+     * Checking whether the using static urls in WYSIWYG allowed event
+     *
+     * @param \Magento\Framework\Event\Observer $observer
+     * @return void
+     */
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $request = $observer->getEvent()->getData('request');
+//        $request->setModuleName('catalog');
+//        $request->setControllerName('product');
+//        $request->setActionName('view');
+        $request->setParams(array('id' => 1));
+    }
+}
