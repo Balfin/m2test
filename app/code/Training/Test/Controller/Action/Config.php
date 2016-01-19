9b32a7eb451b947e98388995d589cf6c28e4b281
Alexey <alexey@Alexeys-MacBook-Pro.local>
2016-01-20 01:15:23 +0200

    Unit2: Hello World Controller

--- app/code/Training/Test/Controller/Action/Config.php
+++ app/code/Training/Test/Controller/Action/Config.php
@@ -4,6 +4,7 @@
  *
  * @copyright Copyright (c) 2014 X.commerce, Inc. (http://www.magentocommerce.com) */
 namespace Training\Test\Controller\Action;
+
 class Config extends \Magento\Framework\App\Action\Action
 {
 
