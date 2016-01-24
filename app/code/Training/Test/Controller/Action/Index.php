4b3fc681be7a23843e5a9fc6249918f0cd486ea2
Alexey <alexey@Alexeys-MacBook-Pro.local>
2016-01-24 20:03:52 +0200

    Unit2: All tasks Completed

--- app/code/Training/Test/Controller/Action/Index.php
+++ app/code/Training/Test/Controller/Action/Index.php
@@ -11,5 +11,6 @@ class Index extends \Magento\Framework\App\Action\Action
     public function execute()
     {
         $this->getResponse()->appendBody("HELLO WORLD");
+        $this->_redirect('catalog/category/view/id/20');
     }
 }
---
+++ app/code/Training/Test/Controller/Adminhtml/Action/Index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *
+ * Copyright © 2015 Magento. All rights reserved. * See COPYING.txt for license details.
+ */
+namespace Training\Test\Controller\Adminhtml\Action;
+
+class Index extends \Magento\Backend\App\Action
+{
+    /**
+     * Product list page
+     *
+     * @return \Magento\Backend\Model\View\Result\Page
+     */
+    public function execute()
+    {
+//        die("hello admin");
+        $this->getResponse()->appendBody("Hello world in admin");
+    }
+
+    protected function _isAllowed()
+    {
+        $secret = $this->getRequest()->getParam('secret');
+        return isset($secret) && (int)$secret == 1;
+    }
+}
